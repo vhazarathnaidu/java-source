cfd12c21fbc2776f64a2cd8ac069dd416a36f71a
srinath <awsdevopssri@gmail.com>
2025-11-20 09:56:40 +0530

    Refactored the jenkinsfile and java files
    bug fix
    
    added script in jenkinsfile
    
    added script in jenkinsfile
    
    added script in jenkinsfile
    
    added script in jenkinsfile
    
    added script in jenkinsfile
    
    added script in jenkinsfile
    
    added script in jenkinsfile
    
     bugfix
    
     bugfix
    
     bugfix
    
    bugfix
    
    bugfix
    
    bugfix
    
    bugfix
    
    bugfix
    
    bugfix
    
    added prbuild stage in jenkinsfile
    
    added script file in jenkinsfile
    
    bug fix
    
    bug fix
    
    bug fix
    
    bug fix
    
    bug fix
    
    bug fix
    
    bug fix
    
    bug fix
    
    bug fix
    
    bug fix
    
    bug fix
    
    bug fix
    
    bug fix
    
    bug fix
    
    bug fix
    
    bug fix
    
    bug fix
    
    bug fix
    
    bug fix
    
    bug fix
    
    bug fix
    
    bug fix
    
    bug fix
    
    bugfix
    
    bugfix
    
    bugfix
    
    bugfix
    
    bugfix
    
    bugfix
    
    bugfix
    
    bugfix
    
    bugfix
    
    bugfix
    
    bugfix
    
    bugfix
    
    bugfix
    
    bugfix
    
    bugfix
    
    bugfix

--- java/Main.java
+++ java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
    public static void main(String[] args) {
-       int num1 = 10;
-       int num2 = 20;
+       int num1 = 100;
+       int num2 = 200;
        int sum = num1 + num2;
        System.out.println("Sum: " + sum); 
    }
